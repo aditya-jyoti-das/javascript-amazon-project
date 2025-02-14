69bd7df16fa84ef795b51aab93819edd37f9b85f
aditya-jyoti-das <adityajyotidas435@gmail.com>
2025-02-14 17:53:51 +0530

    sending All cart ops to cart.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,8 +1,5 @@
-import { cartDict as cartDict } from './cart.js';
+import { AddToCartFun } from './cart.js';
 import { products as products } from '../data/products.js';
-
-
-let countAddToCartProduct = document.querySelector('.cart-quantity');
 
 renderingProduct();
 
@@ -17,31 +14,6 @@ AddToCartFun();
 
 
 
-
-
-
-
-
-
-function AddToCartFun() {
-  document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-    button.addEventListener('click', (event) => {
-      // let productId = event.target.getAttribute('data-product-id');
-
-
-      const productId = button.dataset.productId;
-      let quantitySelectElement = getSiblingByClass(button, 'product-quantity-container');
-
-      AddItemToCart({
-        productId,
-        quantity: Number(quantitySelectElement.children[0].value)
-      })
-      countAddToCartProduct.innerHTML = totalCount();
-
-    });
-  });
-
-}
 function renderingProduct() {
 
   let productContainers = ``;
@@ -96,30 +68,3 @@ function renderingProduct() {
   });
   document.querySelector('.products-grid').innerHTML = productContainers;
 }
-function AddItemToCart(ItemObj) {
-  if (cartDict[ItemObj.productId] != null) {
-    cartDict[ItemObj.productId].quantity += ItemObj.quantity;
-  }
-  else {
-    cartDict[ItemObj.productId] = ItemObj;
-  }
-
-}
-function totalCount() {
-  let totalCount = 0;
-  for (let key in cartDict) {
-    totalCount += cartDict[key].quantity;
-  }
-  return totalCount;
-}
-function getSiblingByClass(element, targetClass) {
-  const parent = element.parentNode;
-  const siblings = parent.children;
-  for (let i = 0; i < siblings.length; i++) {
-    if (siblings[i] !== element && siblings[i].classList.contains(targetClass)) {
-      return siblings[i];
-    }
-  }
-  return null;
-}
-
--- scripts/cart.js
+++ scripts/cart.js
@@ -1,1 +1,51 @@
 export let cartDict = {}
+
+
+
+let countAddToCartProduct = document.querySelector('.cart-quantity');
+
+export function AddToCartFun() {
+    document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+        button.addEventListener('click', (event) => {
+            // let productId = event.target.getAttribute('data-product-id');
+
+
+            const productId = button.dataset.productId;
+            let quantitySelectElement = getSiblingByClass(button, 'product-quantity-container');
+
+            AddItemToCart({
+                productId,
+                quantity: Number(quantitySelectElement.children[0].value)
+            })
+            countAddToCartProduct.innerHTML = totalCount();
+
+        });
+    });
+
+}
+function AddItemToCart(ItemObj) {
+    if (cartDict[ItemObj.productId] != null) {
+        cartDict[ItemObj.productId].quantity += ItemObj.quantity;
+    }
+    else {
+        cartDict[ItemObj.productId] = ItemObj;
+    }
+
+}
+function totalCount() {
+    let totalCount = 0;
+    for (let key in cartDict) {
+        totalCount += cartDict[key].quantity;
+    }
+    return totalCount;
+}
+function getSiblingByClass(element, targetClass) {
+    const parent = element.parentNode;
+    const siblings = parent.children;
+    for (let i = 0; i < siblings.length; i++) {
+        if (siblings[i] !== element && siblings[i].classList.contains(targetClass)) {
+            return siblings[i];
+        }
+    }
+    return null;
+}
